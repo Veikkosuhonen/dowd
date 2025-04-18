cc91f6c5c71a8646c8409d5d418a4301fe71cb3e
Veikko Suhonen <vesuvesuh@gmail.com>
2025-04-19 00:47:38 +0200

    1.13

--- todoapp/src/client/src/App.jsx
+++ todoapp/src/client/src/App.jsx
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
 import styles from './App.module.css';
-import { createResource } from 'solid-js';
+import { For, createResource, createSignal } from 'solid-js';
+
+const [todos, setTodos] = createSignal([
+  { id: 1, text: 'Learn SolidJS', completed: false },
+  { id: 2, text: 'Build a Todo App', completed: false },
+]);
 
 function App() {
   const [hash] = createResource(async () => {
     const res = await fetch('/api/hash')
     return res.text()
   })
+
+  const [newTodo, setNewTodo] = createSignal('');
 
   return (
     <div class={styles.App}>
@@ -16,6 +22,29 @@ function App() {
           {hash()}
         </p>
       </header>
+      <main>
+        <h1>The project App</h1>
+        <ul>
+          <For each={todos()}>
+            {(todo) => (
+              <li>
+                <input type="checkbox" checked={todo.completed} />
+                {todo.text}
+              </li>
+            )}
+          </For>
+        </ul>
+        <div>
+          <input type="text" value={newTodo()} onInput={(e) => setNewTodo(e.target.value)} maxLength={140}/>
+          <button onClick={() => {
+            const newTodos = [...todos(), { id: Date.now(), text: newTodo(), completed: false }];
+            setTodos(newTodos);
+            setNewTodo('');
+          }}>
+            Add
+          </button>
+        </div>
+      </main>
     </div>
   );
 }
